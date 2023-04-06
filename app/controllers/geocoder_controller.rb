ca61a9c48178ff9e6a2321d0d36da06589e24a76
bilunkocaaslan <bilunkocaaslan@hotmail.com>
2023-04-07 00:58:12 +0300

    class variables descprition fixed

--- app/controllers/geocoder_controller.rb
+++ app/controllers/geocoder_controller.rb
@@ -4,33 +4,33 @@ require 'kmeans-clusterer'
 class GeocoderController < ApplicationController
 
     def index 
-        @no_of_basket = 5
-        @no_of_point = 15
-        @points = []
-        @baskets = Array.new(@no_of_basket) { [] }
-        @center_loc = Geocoder.search("Gaziemir, Izmir")
-        @center_point = Geocoder::Calculations.geographic_center([[@center_loc[0].latitude, @center_loc[0].longitude]])
-        @center_lat = @center_point[0]
-        @center_lng = @center_point[1]
+        @@no_of_basket = 5
+        @@no_of_point = 20
+        @@points = []
+        @@baskets = Array.new(@@no_of_basket) { [] }
+        @@center_loc = Geocoder.search("Gaziemir, Izmir")
+        @@center_point = Geocoder::Calculations.geographic_center([[@@center_loc[0].latitude, @@center_loc[0].longitude]])
+        @center_lat = @@center_point[0]
+        @center_lng = @@center_point[1]
         puts "#{generate_point}"
     end
 
     def generate_point
          # for generating random test points in Gaziemir
-        for i in 1..@no_of_point do
+        for i in 1..@@no_of_point do
             lat, lng = Geocoder::Calculations.random_point_near([@center_lat, @center_lng], 5) # generates a random point within 5km of Gaziemir
-            @points.append([lat, lng])
+            @@points.append([lat, lng])
         end
-        puts "#{@points.length()} random points generated"
+        puts "#{@@points.length()} random points generated"
         puts "#{cluster_point}"
 
     end
 
     def cluster_point
-        kmeans = KMeansClusterer.run @no_of_basket, @points, labels: "labels", runs: 1
-        @points.each_with_index do |point, i|
+        kmeans = KMeansClusterer.run @@no_of_basket, @@points, labels: "labels", runs: 1
+        @@points.each_with_index do |point, i|
             idx = kmeans.predict([point]).first
-            new_basket = @baskets[idx]
+            new_basket = @@baskets[idx]
             if new_basket.length() > 0
                 new_basket.each do |p|
                     distance = Geocoder::Calculations.distance_between(point, p, units: :km)
@@ -38,7 +38,7 @@ class GeocoderController < ApplicationController
                         new_basket.append(point)
                         break
                     else
-                        @baskets.append([point])
+                        @@baskets.append([point])
                     end
                     end
             else
@@ -52,7 +52,7 @@ class GeocoderController < ApplicationController
     def write_file
         file = File.open("results.txt", "w")
         results = []
-        @baskets.each_with_index do |basket,i|
+        @@baskets.each_with_index do |basket,i|
             puts "BASKET_NUM#{i+1}"
             file.write("BASKET_NUM#{i+1}")
             basket.each_with_index do |location, j|
